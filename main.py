48af179133a6b6e58c554f0541f92afa44479dc6
Suki <sukileung246@gmail.com>
2021-01-04 10:17:34 -0800

    commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+# midnight rider
+# a text adventure game that is riveting.
+# IGN gives it 4 stars out of 100
+
+def main():
+    pass # doesnt do anything
+    # Show the introduction
+
+    # loop
+    # TODO: check if reached END GAME
+    # TODO: Give players their choices
+
+    # TODO: change the environment based on choice and RNG
+
+
+if __name__ == '__main__':
+    main()
